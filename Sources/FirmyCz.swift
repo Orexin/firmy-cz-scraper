695857615ed6834462657114fc447c9dae1db759
Filip Sikora <contact@fsikora.com>
2023-06-12 03:33:25 +0200

    huh

--- Sources/FirmyCz.swift
+++ Sources/FirmyCz.swift
@@ -1,6 +1,7 @@
 import Foundation
 import ArgumentParser
 
+@available(iOS 13.4, *)
 @main
 struct FirmyCz: ParsableCommand {
     @Option(name: .shortAndLong, help: "page limit, default: 150")
@@ -17,15 +18,39 @@ struct FirmyCz: ParsableCommand {
     
     mutating func run() throws {
         query = query.replacingOccurrences(of: " ", with: "+")
-        let scraper = Scraper.init(query: query, pageLimit: limit)
-        let data: [[String]] = try scraper.scrape()
-        
-        for d in data {
-            print(d)
-        }
+        try write(try getFile(), try Scraper(query: query, pageLimit: limit).scrape())
     }
     
-    func write() throws {
+    private func getFile() throws -> FileHandle {
+        var filePath = FileManager().currentDirectoryPath + "/data.csv"
         
+        if output != nil {
+            filePath = output!
+        }
+        
+        if !FileManager().fileExists(atPath: filePath) {
+            FileManager().createFile(atPath: filePath, contents: nil)
+        }
+        
+        let file = FileHandle(forWritingAtPath: filePath)
+        
+        if !append {
+            try "".write(toFile: filePath, atomically: false, encoding: .utf8)
+        }
+        
+        return file!
+    }
+    
+    private func write(_ file: FileHandle, _ data: [[String]]) throws {
+        try file.seekToEnd()
+        for row in data {
+            for (i, col) in row.enumerated() {
+                try file.write(contentsOf: col.data(using: .utf8)!)
+                if i != row.count {
+                    try file.write(contentsOf: ";".data(using: .utf8)!)
+                }
+            }
+            try file.write(contentsOf: "\n".data(using: .utf8)!)
+        }
     }
 }
